c97cbd02a83237b2b8ea9f55283cd630f3e04f8f
Dima Smirnov <email@dimasmirnov.ru>
2017-04-30 00:09:10 +0300

    ref

--- api/index.js
+++ api/index.js
@@ -18,9 +18,7 @@ app.get('/action', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+  socket.on('disconnect', () => console.log('user disconnected'));
 });
 
 http.listen(3000, () => console.log('listening on *:3000'));
