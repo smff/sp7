165b60f3936ae14d84014d60c21a567dfe3dc722
Dima Smirnov <email@dimasmirnov.ru>
2017-04-30 00:08:34 +0300

    added sockets

--- api/index.js
+++ api/index.js
@@ -2,36 +2,25 @@
 
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const handSensorProcessor = require('./handSensorProcessor');
 
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
 app.get('/', (req, res) => {
-  let action = handSensorProcessor(req.query);
-  res.send(action);
+  res.sendFile(__dirname + '/index.html');
 });
 
-
-// io.on('connection', function(socket) {
-//   console.log('a user connected');
-//   socket.on('disconnect', function() {
-//     console.log('user disconnected');
-//   });
-// });
-//
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//     console.log('message: ' + msg);
-//   });
-// });
-
-
-// http.listen(3000, function() {
-//   console.log('listening on *:3000');
-// });
-
-app.listen(3000, function() {
-  console.log('listening on *:3000');
+app.get('/action', (req, res) => {
+  io.emit('add action', handSensorProcessor(req.query));
+  res.sendStatus(200);
 });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => console.log('listening on *:3000'));
